bb36024e68e43f4c7b32ed341fdf17aab560cd1f
Kauanoliveirads <kauanoliveiradossantos2003@gmail.com>
2025-05-29 13:36:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Kauanoliveirads/image-processing-package",
+    url="https://github.com/Kauanoliveirads/DIO-image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.10',
